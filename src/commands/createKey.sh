9a0ab3fb74149bcc0f09bcceaa831114c542d6d1
John Mutua Makumi <johnmutuamakumi1@gmail.com>
2025-06-25 22:08:02 +0300

    refactor

--- src/commands/createKey.sh
+++ src/commands/createKey.sh
@@ -2,8 +2,8 @@
 
 echo "Creating SSH key..."
 TASK="create"
-TS_FILE_LOCATION="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-TS_FILE="${TS_FILE_LOCATION}/createKey.ts"
+CONTAINING_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+TS_FILE="${CONTAINING_DIR}/createKey.ts"
 TEMP_FILE="/tmp/sship/sship-${TASK}-responses.json"
 
 echo "Running TypeScript file to get key details..."
@@ -18,6 +18,12 @@ PASSPHRASE="$(jq -r '.passphrase' < "${TEMP_FILE}")"
 
 echo "Extracting key name from input..."
 NAME="$(jq -r '.name' < "${TEMP_FILE}")"
+
+echo "Extracting host from input..."
+HOST="$(jq -r '.host' < "${TEMP_FILE}")"
+
+echo "Extracting user from input..."
+MACHINE_USER="$(jq -r '.user' < "${TEMP_FILE}")"
 # Check if required fields are present (POSIX compliant)
 if [ -z "${EMAIL}" ] || [ "${EMAIL}" = "null" ]; then
     echo "Error: 'email' is missing in ${TEMP_FILE}."
@@ -34,7 +40,21 @@ if [ -z "${NAME}" ] || [ "${NAME}" = "null" ]; then
     exit 1
 fi
 
+if [ -z "${MACHINE_USER}" ] || [ "${MACHINE_USER}" = "null" ]; then
+    echo "Error: 'user' is missing in ${TEMP_FILE}."
+    exit 1
+fi
+
+if [ -z "${HOST}" ] || [ "${HOST}" = "null" ]; then
+    echo "Error: 'host' is missing in ${TEMP_FILE}."
+    exit 1
+fi
+
 echo "Generating SSH key with name: ${NAME}"
 ssh-keygen -t ed25519 -C "${EMAIL}" -N "${PASSPHRASE}" -f ~/".ssh/${NAME}"
 
 echo "SSH key creation complete."
+
+echo "Adding SSH configuration for key to config..."
+
+"$CONTAINING_DIR/sshConf.sh" "${NAME}" "${MACHINE_USER}" "${HOST}"
--- src/commands/deleteKey.ts
+++ src/commands/deleteKey.ts
@@ -1,24 +1,39 @@
+import { getAllFiles } from "../getAllFiles.ts";
 import { select } from "../select.ts";
+import { getKeys } from "../getKeys.ts";
+
+const args = Deno.args;
+// get files in ssh dir
+const location = args[0];
+const fullLocation = `${location}/.ssh`;
+
+
+
+
+
+function deleteSelectedKey(selectedKey:string, files:string[]){
+    const filesToDelete = files.filter((file) => file.includes(selectedKey));
+
+  filesToDelete.forEach((file) => {
+    console.log(`Deleting file: ${file}`);
+    const filePath = `${fullLocation}/${file}`;
+
+    Deno.removeSync(filePath);
+    console.log(`Deleted file: ${filePath}`);
+  });
+
+  console.log(`You selected: ${selectedKey}`);
+
+}
 
 async function deleteKey() {
-  const args = Deno.args;
-  // get files in ssh dir
-  const location = args[0];
-  const fullLocation = `${location}/.ssh`;
-  const filesIterator = Deno.readDirSync(fullLocation);
-  const files = Array.from(filesIterator).filter((file) => file.isFile).map((
-    file,
-  ) => file.name);
-  const idKeys = files.filter((file) => {
-    return (file.endsWith(".pub") || file.endsWith(".pem") ||
-      file.endsWith(".pkcs8"));
-  });
-  const pairName = idKeys.map((file) => {
-    const fileNameParts = file.split(".");
-    return fileNameParts[0];
-  });
+  const pairNames = getKeys(getAllFiles(fullLocation));
+  if (pairNames.length === 0) {
+    console.log("No keys found to delete");
+    return;
+  }
 
-  const selectedKey = await select("Select a key to delete", pairName);
+  const selectedKey = await select("Select a key to delete", pairNames);
 
 //   prompt the user for confirmation before deleting
 
@@ -31,17 +46,9 @@ async function deleteKey() {
     return;
   }
 
-  const filesToDelete = files.filter((file) => file.includes(selectedKey));
+  deleteSelectedKey(selectedKey, getAllFiles(fullLocation));
 
-  filesToDelete.forEach((file) => {
-    console.log(`Deleting file: ${file}`);
-    const filePath = `${fullLocation}/${file}`;
 
-    Deno.removeSync(filePath);
-    console.log(`Deleted file: ${filePath}`);
-  });
-
-  console.log(`You selected: ${selectedKey}`);
 }
 
 await deleteKey();
