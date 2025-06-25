63ad351a13e1db3d50bc795eb398335668d561da
John Mutua Makumi <johnmutuamakumi1@gmail.com>
2025-06-25 22:08:28 +0300

    refactor

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,12 @@ export type UserPromptMessage = {
 };
 
 export type tempWriteTasks = "create";
+
+export type SshConfTemplate = {
+  alias:string
+  hostname:string
+  user:string
+  port:number
+  identityFile:string
+  identitiesOnly:'yes' | 'no';
+}
