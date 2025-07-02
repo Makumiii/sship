f2202febf6ac6c255368e123230e14e2ece6671a
John Mutua Makumi <johnmutuamakumi1@gmail.com>
2025-07-02 23:21:20 +0300

    chore: simplify README description

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,17 @@
 import { writeResponses } from "./io.ts";
 import { select } from "./select.ts";
+import { ExitPromptError } from "npm:@inquirer/core";
 
 const appTasks = ["create", "delete", "backup", "list", 'uninstall'];
 
-const chosenTask = await select("What do you want to do?", appTasks);
-writeResponses({ chosenTask }, "task");
+try {
+  const chosenTask = await select("What do you want to do?", appTasks);
+  writeResponses({ chosenTask }, "task");
+} catch (error) {
+  if (error instanceof ExitPromptError) {
+    console.log("\nAborted. Exiting...");
+    Deno.exit(130);
+  } else {
+    console.error("An unexpected error occurred:", error);
+  }
+}
