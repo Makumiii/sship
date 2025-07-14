29b027fbed26763983539551fe9c5b0358ac01ec
John Mutua Makumi <johnmutuamakumi1@gmail.com>
2025-07-14 16:13:58 +0300

    feat: add backup functionality

--- src/commands/backup.sh
+++ src/commands/backup.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SSH_FOLDER_LOCATION="$HOME/.ssh"
-BACKUP_DIR="/tmp/sship-backup"
+BACKUP_DIR="${1}"
 ARCHIVE_NAME="$HOME/sship_backup.tar.gz"
-PASS_PHRASE="${1:-}"
+PASS_PHRASE="${2:-}"
 
-rm -rf "$BACKUP_DIR"
-mkdir -p "$BACKUP_DIR"
 
-find "$SSH_FOLDER_LOCATION" -maxdepth 1 -type f \( \
-    -name "id_*" -o -name "*.pub" -o -name "config" \
-\) ! \( \
-    -name "known_hosts" -o -name "authorized_keys" -o -name "*.sock" -o -name "control" -o -name "*.pid" \
-\) -exec cp {} "$BACKUP_DIR" \;
 
 tar -czf "$ARCHIVE_NAME" -C "$BACKUP_DIR" .
 
